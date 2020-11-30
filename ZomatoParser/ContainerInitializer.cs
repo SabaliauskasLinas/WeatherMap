0a1a7e5e74c373e5c4655969c361d68f0f0a47e5
Linas <linas.sabaliauskas@tglab.com>
2020-11-30 15:35:44 +0200

    (ZOM-1) Init

---
+++ ZomatoParser/ContainerInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZomatoParser
+{
+    public static class ContainerInitializer
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            return builder.Build();
+        }
+    }
+}
---
+++ ZomatoParser/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ZomatoParser
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.ListView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // ListView
+            // 
+            this.ListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListView.HideSelection = false;
+            this.ListView.Location = new System.Drawing.Point(12, 12);
+            this.ListView.Name = "ListView";
+            this.ListView.Size = new System.Drawing.Size(467, 306);
+            this.ListView.TabIndex = 0;
+            this.ListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "Zomato";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView ListView;
+    }
+}
+
